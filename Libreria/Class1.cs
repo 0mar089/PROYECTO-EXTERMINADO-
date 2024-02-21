59b9ceb4e03e8db2f84404c01953f355d780f9e3
Marcvg05 <marcvg090909@gmail.com>
2024-02-21 10:41:37 +0100

    Avion clase

--- Libreria/Class1.cs
+++ Libreria/Class1.cs
@@ -1,7 +1,35 @@
-﻿namespace Libreria
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Libreria
 {
     public class Avion
     {
+        string ID;
+        double ox;
+        double oy;
+        double dx;
+        double dy;
+        double velocidad;
+        double px;
+        double py;
+        public Avion(string id, double ox, double oy, double dx, double dy, double velocidad, double px, double py)
+        {
+            this.ID = id;
+            this.ox = ox;
+            this.oy = oy;
+            this.dx = dx;
+            this.dy = dy;
+            this.velocidad = velocidad;
+            this.px = px;
+            this.py = py;
+        }
+
+        
+
 
     }
 }
